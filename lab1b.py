2db845bca405853519a3566dbf9b00d213971002
ishmeetkaur3 <156566750+ishmeetkaur3@users.noreply.github.com>
2024-02-12 22:15:13 -0500

    Add files via upload

---
+++ lab1b.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+lab1b.py - Python Script with Objects
+
+Description:
+This script uses objects to print a personalized message.
+
+Author: Ishmeet Kaur
+Date: January 15, 2024
+"""
+
+# Define objects
+name = "Isaac"
+age = 40
+
+# Print a personalized message
+print(f"How are you {name}? Happy {age}th birthday!")
